d899be21a70d2eedb8069c63722dfeec51ab3670
frankolombo1234 <78540725+frankolombo1234@users.noreply.github.com>
2022-09-22 10:15:32 +0200

    Add files via upload

---
+++ PZW-JS/script.js
@@ -0,0 +1,30 @@
+function pokaziDatum() {
+
+
+
+    datum = document.getElementById("datum");
+    datum.innerHTML = Date();
+
+
+
+
+
+}
+
+function pokaziGodinu() {
+
+
+
+    godina = document.getElementById("godina");
+    godina.innerHTML = new Date().getFullYear();
+
+
+
+
+
+}
+
+
+
+
+
